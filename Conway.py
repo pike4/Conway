d9a065690d0720bdc2a23ce5257b1ccb2800a257
pike4 <pike4@purdue.edu>
2016-04-27 11:06:37 -0400

    Rows and columns were backwards

--- Conway.py
+++ Conway.py
@@ -1,28 +1,28 @@
 def getNeighborCount(x, y, Matrix):
     count = 0
     if x > 0:
-        if Matrix[x - 1][y] == 1:
+        if Matrix[y][x-1] == 1:
             count+=1
         if y > 0:
-            if Matrix[x - 1][y - 1] == 1:
+            if Matrix[y - 1][x - 1] == 1:
                 count+=1
         if y < h - 1:
-            if Matrix[x - 1][y + 1] == 1:
+            if Matrix[y + 1][x - 1] == 1:
                 count+=1
     if x < w - 1:
-        if Matrix[x + 1][y] == 1:
+        if Matrix[y][x + 1] == 1:
             count+=1
         if y > 0:
-            if Matrix[x + 1][y - 1] == 1:
+            if Matrix[y - 1][x + 1] == 1:
                 count+=1
         if y < h - 1:
-            if Matrix[x + 1][y + 1] == 1:
+            if Matrix[y + 1][x + 1] == 1:
                 count+=1
     if y > 0:
-        if Matrix[x][y - 1] == 1:
+        if Matrix[y - 1][x] == 1:
             count+=1
     if y < h - 1:
-        if Matrix[x][y + 1] == 1:
+        if Matrix[y + 1][x] == 1:
             count+=1
 
     return count
@@ -31,13 +31,13 @@ def getNeighborCount(x, y, Matrix):
 def decideCellValue(x, y, Matrix):
     neighbors = getNeighborCount(x, y, Matrix)
 
-    if Matrix[x][y] == 1:
+    if Matrix[y][x] == 1:
         if neighbors > 3 or neighbors < 2:
             return 0
         else:
             return 1
 
-    elif Matrix[x][y] == 0:
+    elif Matrix[y][x] == 0:
         if neighbors == 3:
             return 1
         else:
@@ -53,11 +53,17 @@ decisionList = list()
 
 Board =[[0 for x in range(0,w)] for y in range(0,h)];
 
+Board[33][30] = 1;
+Board[33][31] = 1;
+Board[33][32] = 1;
 Board[33][33] = 1;
-Board[35][33] = 1;
-Board[35][32] = 1;
-Board[35][34] = 1;
-Board[34][34] = 1;
+Board[33][34] = 1;
+Board[33][35] = 1;
+Board[33][36] = 1;
+Board[33][37] = 1;
+Board[33][38] = 1;
+Board[33][39] = 1;
+
 
 for r in range (0, 20):
     outString = ""
